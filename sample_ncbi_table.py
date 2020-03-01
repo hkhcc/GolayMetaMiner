bb296d0b86aeee2ebafd3713fb05bbfce521e60b
hkhcc <functionalgroup@gmail.com>
2020-03-01 12:18:46 +0800

    Added sample_ncbi_table.py helper script
    A sample output, mycobacterium_short.csv was generated (random seed 42, sample size 10) and included with
    the commit.

---
+++ sample_ncbi_table.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+sample_ncbi_table.py: a helper script for downsampling of NCBI genome lists
+
+@author: Tom C.C. HO 
+"""
+import random
+import sys
+
+if __name__ == '__main__':
+    table_path = sys.argv[1]
+    sample_size = int(sys.argv[2])
+    random_seed = int(sys.argv[3])
+    print('# Sampling from', table_path, 'a total of', sample_size, 'rows', file=sys.stderr)
+    print('# Random seed:', random_seed, file=sys.stderr)
+    output_array = []
+    all_content_rows = []
+    # read the genome list
+    with open(table_path, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                output_array.append(line)
+            elif line.rstrip('\r').rstrip('\n') == '':
+                pass
+            else:
+                all_content_rows.append(line)
+    # perform the sampling
+    random.seed(random_seed)
+    output_array += random.sample(all_content_rows, sample_size)
+    # output the result
+    for row in output_array:
+        print(row, end='', file=sys.stderr)
+    
+
